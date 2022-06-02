91e4c4938b6e262d191ac287d464cb6bd3469434
richard gichuki <chadgichuki@gmail.com>
2022-06-02 21:15:00 +0300

    Finihed assignment im index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+
+// Removes the <main> with #main
+const main = document.querySelector('#main');
+main.remove();
+
+// Create a hi#victory node stored in newHeader variable.
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+
+
+// textContent of newHeader node to be "YOUR_NAME is the champion"
+newHeader.textContent = "YOUR-NAME is the champion";
+
+// I guess we should append the element to the body
+document.body.append(newHeader)
